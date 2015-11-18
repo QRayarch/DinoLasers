9dc27180159dd96d79894657022e2db4acefc4e2
unknown <mf7753@NM2550-15.main.ad.rit.edu>
2015-11-18 18:40:19 -0500

    Now fires using mouse button left instead of SPACE.

--- DinoLasers/TestLevel.cpp
+++ DinoLasers/TestLevel.cpp
@@ -22,7 +22,7 @@ void TestLevel::Load() {
 	//dino->AddComponent(new Rigidbody());
 	dino->AddComponent(new Health());
 	dino->AddComponent(new PlayerMovement());
-	//dino->GetTransform().SetPosition(vector3(0.0f, 5.0f, 0.0f));
+	dino->GetTransform().SetPosition(vector3(0.0f, 5.0f, 0.0f));
 	dino->AddComponent(new ProjectileLauncher());
 	//dino->AddComponent(new CollisionDebug());
 	GameObjectManager::GetInstance()->AddGameObject(dino);
